7007e2c8972445a870a8595dd01db1dd6f3004d4
Vinh Le <vinh.ninja@icloud.com>
2018-12-04 09:52:51 +0800

    Add sublime scripts

---
+++ configs/sublime/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings
+cp Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Package\ Control.sublime-settings
+cp Default\ (OSX).sublime-keymapime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Default\ (OSX).sublime-keymapime-settings
