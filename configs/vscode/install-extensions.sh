78de777e1c473d170604f4f8fc0c6068f87d3f08
Vinh Le <vinh.ninja@icloud.com>
2018-12-04 09:44:28 +0800

    Correct Code Insiders path

--- configs/vscode/install-extensions.sh
+++ configs/vscode/install-extensions.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # Generated via
-# code --list-extensions | xargs -L 1 echo code --install-extension
+# code-insiders --list-extensions | xargs -L 1 echo code-insiders --install-extension
 
-code --install-extension albymor.increment-selection
-code --install-extension bbenoist.Nix
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension eamodio.gitlens
-code --install-extension eg2.vscode-npm-script
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension jpoissonnier.vscode-styled-components
-code --install-extension kumar-harsh.graphql-for-vscode
-code --install-extension mauve.terraform
-code --install-extension mechatroner.rainbow-csv
-code --install-extension ms-python.python
-code --install-extension ms-vscode.Go
-code --install-extension ms-vscode.sublime-keybindings
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension Orta.vscode-jest
-code --install-extension patrys.vscode-code-outline
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension rafamel.subtle-brackets
-code --install-extension shinnn.stylelint
-code --install-extension waderyan.gitblame
-code --install-extension wayou.vscode-todo-highlight
-code --install-extension wix.vscode-import-cost
-code --install-extension wmaurer.change-case
+code-insiders --install-extension albymor.increment-selection
+code-insiders --install-extension bbenoist.Nix
+code-insiders --install-extension christian-kohler.npm-intellisense
+code-insiders --install-extension christian-kohler.path-intellisense
+code-insiders --install-extension DavidAnson.vscode-markdownlint
+code-insiders --install-extension dbaeumer.vscode-eslint
+code-insiders --install-extension dzannotti.vscode-babel-coloring
+code-insiders --install-extension eamodio.gitlens
+code-insiders --install-extension eg2.vscode-npm-script
+code-insiders --install-extension Equinusocio.vsc-material-theme
+code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension formulahendry.auto-rename-tag
+code-insiders --install-extension jpoissonnier.vscode-styled-components
+code-insiders --install-extension kumar-harsh.graphql-for-vscode
+code-insiders --install-extension mauve.terraform
+code-insiders --install-extension mechatroner.rainbow-csv
+code-insiders --install-extension ms-python.python
+code-insiders --install-extension ms-vscode.Go
+code-insiders --install-extension ms-vscode.sublime-keybindings
+code-insiders --install-extension msjsdiag.debugger-for-chrome
+code-insiders --install-extension Orta.vscode-jest
+code-insiders --install-extension patrys.vscode-code-outline
+code-insiders --install-extension PeterJausovec.vscode-docker
+code-insiders --install-extension rafamel.subtle-brackets
+code-insiders --install-extension shinnn.stylelint
+code-insiders --install-extension waderyan.gitblame
+code-insiders --install-extension wayou.vscode-todo-highlight
+code-insiders --install-extension wix.vscode-import-cost
+code-insiders --install-extension wmaurer.change-case
 
 cp ./settings.json $HOME/Library/Application\ Support/Code\ Insiders/User/settings.json
