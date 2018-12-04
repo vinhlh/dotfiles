481fef33377ca2b27b4929fe57e46e21fc10c483
Vinh Le <vinh.ninja@icloud.com>
2018-12-04 09:55:58 +0800

    Correct paths

--- configs/sublime/install.sh
+++ configs/sublime/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings
-cp Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Package\ Control.sublime-settings
-cp Default\ (OSX).sublime-keymapime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Default\ (OSX).sublime-keymapime-settings
+cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/
+cp Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/
+cp Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text*/Packages/User/
